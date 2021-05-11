6c722c3b10708c15bfea0b81475d5c419bf41965
Max Shabarchin <maks.shabarchin@yandex.ru>
2021-05-11 11:53:21 +0300

    Версия 1.0

---
+++ Sources/PotPirate Utility/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿#region Using directives
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+[assembly: AssemblyTitle("PotPirate Utility")]
+[assembly: AssemblyDescription("Утилита настройки PotPlayer для просмотра торрент-фильмов без скачивания торрента")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Zalexanninev15")]
+[assembly: AssemblyProduct("PotPirate Utility")]
+[assembly: AssemblyCopyright("GPL-3.0 License")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
